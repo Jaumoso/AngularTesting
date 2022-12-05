865ef684930e42453a1f4e320fc276a934f72682
Jaume Civera Soriano <jaumecvr2000@gmail.com>
2022-12-05 13:56:13 +0100

    select dishes

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import { Comment } from './comment';
 export class Dish {
 
     id: string;
@@ -8,5 +9,6 @@ export class Dish {
     label: string;
     price: string;
     description: string;
+    comments: Comment[];
 
 }
