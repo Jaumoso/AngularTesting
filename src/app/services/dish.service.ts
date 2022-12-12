81d51054ff4184c5946ffa4a4619e3fb05284f8e
Jaume Civera Soriano <jaumecvr2000@gmail.com>
2022-12-12 12:27:31 +0100

    rxJS part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
-import { of, pipe, Observable } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
 @Injectable({
@@ -24,6 +24,10 @@ export class DishService {
   getFeaturedDish(): Observable<Dish> {
       // Simulate server latency with 2 second delay
     return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
+  }
+
+  getDishIds(): Observable<string[] | any>  {
+    return of(DISHES.map((dish) => dish.id));
   }
 
 }
