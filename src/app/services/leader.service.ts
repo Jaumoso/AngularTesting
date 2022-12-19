94e4405257c9b6cc9dd490059a76e947b5248f98
Jaume Civera Soriano <jaumecvr2000@gmail.com>
2022-12-19 09:46:32 +0100

    animations part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -25,7 +25,8 @@ export class LeaderService {
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return this.http.get<Leader>(baseURL + 'leader?featured=true').pipe(map(leaders => leaders[0]))
+    return this.http.get<Leader>(baseURL + 'leaders?featured=true').pipe(map(leaders => leaders[0]))
     .pipe(catchError(this.processHTTPMsgService.handleError));
   }
+
 }
